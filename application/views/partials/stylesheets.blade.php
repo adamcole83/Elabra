46f323210f744303b632387e2649e20d1293a18e
Adam Jenkins <adamcole83@gmail.com>
2013-04-29 16:42:18 -0500

    Updated stylesheets and scripts for Twitter Bootstrap

--- application/views/partials/stylesheets.blade.php
+++ application/views/partials/stylesheets.blade.php
@@ -2,7 +2,3 @@
 {{ HTML::style('css/bootstrap-responsive.css') }}
 
 {{ HTML::style('css/application.css') }}
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-	{{ HTML::style('js/html5shiv.js') }}
-<![endif]-->
